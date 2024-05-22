8bbdb30e29c0aed4e33d2e5ff815bfd6f3f5f836
Bildad0 <bildadowuor@gmail.com>
2024-05-22 23:57:48 +0300

    updates and fixes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,11 @@
-
+import Logo from '../assets/png/logo-no-background.png'
 
 function Header() {
     return (
         <header className="bg-white shadow">
             <div className="container mx-auto px-6 py-4">
                 <div className="flex items-center justify-between">
-                    <div className="text-lg font-bold text-gray-700">My Blog</div>
+                    <div className="text-lg font-bold text-gray-700"><img src={Logo} alt='Logo' loading='lazy' width={250}/></div>
                     <nav className="flex space-x-4">
                         <a href="/" className="text-gray-700 hover:text-gray-900">Home</a>
                         <a href="#" className="text-gray-700 hover:text-gray-900">About</a>
